5ededc61780faea6590634fb8499b9ce68f201f9
lion <vinayy.@gmail.com>
2023-01-13 22:05:17 +0530

    First time

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { CiAirportSign1, CiAlarmOn } from "react-icons/ci";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <h1>Airfort <CiAirportSign1/></h1>
+  <h1>AlarmOn <CiAlarmOn /></h1>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
